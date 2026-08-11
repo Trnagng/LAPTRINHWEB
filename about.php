f798dc50475bb39daffa00b358ae332fd6ab538e
Trnagng <changnguyn110@gmail.com>
2026-08-11 19:32:07 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Giới thiệu bản thân</title>
+</head>
+<body>
+
+    <h1>Giới thiệu bản thân</h1>
+
+    <p>Xin chào, mình là Trang.</p>
+    <p>Mình đang học lập trình web với PHP.</p>
+
+    <h2>Dự án đã thực hiện</h2>
+    <ul>
+        <li>Website du lịch</li>
+        <li>Website đặt tour</li>
+    </ul>
+
+</body>
+</html>
